2c0bce676de39df871ba9262ead6652831815291
bananabread08 <rmkfermin@gmail.com>
2021-01-19 17:53:01 +0800

    added Resize button, added button event listener, and set a size for main container

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 An Etch-a-Sketch project as part of the curriculum of The Odin Project. This is to flex the materials we had learned in the DOM Manipulation Lesson and to Vanilla Javascript as a whole.
 
-v.1: 16x16 grid. Can initially "sketch". 
+v.1: 16x16 grid. Can initially "sketch".  
+
+v.2: Ability to resize grid. Limited to a value up to 100. Higher values can cause lag in browser.  
+
+What I learned:  
+1. The use of `setProperty` to manipulate CSS from the DOM.  
+2. Got a bit comfortable in using event listeners.  
+3. CSS Grid!!
--- scripts/eas.js
+++ scripts/eas.js
@@ -4,18 +4,30 @@ function createGrid(size){
         gridSqr.setAttribute("id", "squares");
         squareWrapper.appendChild(gridSqr);
     }
+    squareWrapper.style.setProperty(
+        "grid-template-columns",
+        `repeat(${size}, 1fr)`
+    );
 }  
-
-const squareWrapper = document.getElementById("container");
-createGrid(16);
-const divs = document.querySelectorAll("div"); //select all created divs from createGrid();
-colorGrid(divs);
 
 function colorGrid(blocks){
     blocks.forEach(div => {
         div.addEventListener("mouseenter", function(){
-            div.style.backgroundColor = "red";
+            div.style.backgroundColor = "cornflowerblue";
         });
     });
 }
 
+const dimension = document.getElementById("size");
+const squareWrapper = document.querySelector("#container");
+const resize = document.querySelector("#resize");
+createGrid(16);
+const divs = document.querySelectorAll("div"); //select all created divs from createGrid();
+colorGrid(divs);
+
+resize.addEventListener("click", function(){ //event for clicking Resize button
+    squareWrapper.innerHTML = "";
+    createGrid(dimension.value);
+    const newdivs = document.querySelectorAll("div");
+    colorGrid(newdivs);
+});
